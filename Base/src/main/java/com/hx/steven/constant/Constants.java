24a332028161d73ade14c2b19c8465869be790c4
wingtech\huanxiao <18122147149@qq.com>
2021-07-15 09:50:17 +0800

    修改框架Router跳转

--- Base/src/main/java/com/hx/steven/constant/Constants.java
+++ Base/src/main/java/com/hx/steven/constant/Constants.java
@@ -1,6 +1,6 @@
 package com.hx.steven.constant;
 
 public class Constants {
-    public static final String A_WELCOME = "/app/welComeActivity";
+    public static final String A_WELCOME = "/main/WelcomeActivity";
     public static final String A_MAIN = "/main/MainActivity";
 }
